d0ab504baec42303bd02550db4041fd2b0bef490
Alex Schickedanz <abcaeffchen@gmail.com>
2017-09-20 12:11:26 +0200

    Moved equation code to header file

--- src/Equation.h
+++ src/Equation.h
@@ -19,7 +19,17 @@ struct Equation
     long counter;
     bool fromContinuedFraction;
 
-    Equation(NTL::Vec<long> e, NTL::ZZ v, long level, double reduced, long round, double time, bool fromContinuedFraction = false);
+    Equation(NTL::Vec<long> e, NTL::ZZ v, long level, double reduced, long round, double time, bool fromContinuedFraction = false)
+        : e(std::move(e)),
+          v(std::move(v)),
+          level(level),
+          reduced(reduced),
+          round(round),
+          time(time),
+          counter(1),
+          fromContinuedFraction(fromContinuedFraction)
+    {}
+
 };
 
 /**
@@ -27,17 +37,65 @@ struct Equation
  * @param right
  * @return bool
  */
-bool operator== (const Equation &left, const Equation &right);
+inline bool operator==(const Equation& left, const Equation& right)
+{
+    return static_cast<bool>(left.e == right.e);    // NTLs vector comparator returns a long with 0 and 1 as only possible value...
+}
+
 
 /**
  * @param left
  * @param right
  * @return bool
  */
-bool operator< (const Equation &left, const Equation &right);
+inline bool operator<(const Equation& left, const Equation& right)
+{
+    long n = left.e.length();
 
-bool sort_equations(const Equation &left, const Equation &right);
+    // if left and right have not the same length
+    if(n < right.e.length())
+        return true;
+    else if(n > right.e.length())
+        return false;
 
-bool sort_equations_by_v(const Equation &left, const Equation &right);
+    // compare the exponents
+    for(long i = 0; i < n; ++i)
+    {
+        if(left.e[i] < right.e[i])
+            return true;
+        else if(left.e[i] > right.e[i])
+            return false;
+    }
+
+    // they are equal
+    return false;
+}
+
+/**
+ * Sorts equations by round and time
+ * @param left
+ * @param right
+ * @return
+ */
+inline bool sort_equations(const Equation& left, const Equation& right)
+{
+    if(left.round < right.round)
+        return true;
+    else if(left.round > right.round)
+        return false;
+
+    return left.time < right.time;
+}
+
+/**
+ * Sorts equations by the value of v
+ * @param left
+ * @param right
+ * @return
+ */
+inline bool sort_equations_by_v(const Equation& left, const Equation& right)
+{
+    return left.v < right.v != 0;
+}
 
 #endif	/* EQUATION_H */
